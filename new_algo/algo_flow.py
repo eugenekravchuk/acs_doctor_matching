8e0e114399087e6d5022f6057d16ffa847646092
Maxim-Vasyl Tarnavskyi <tarnmavas@gmail.com>
2025-05-29 15:31:24 +0300

    Algo and data changes

--- new_algo/algo_flow.py
+++ new_algo/algo_flow.py
@@ -3,25 +3,129 @@ import networkx as nx
 import itertools
 import sys
 import os
-import json 
+import json
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from maximum_flow_impl import min_cost_max_flow
 
+def split_data(data_str, delim=','):
+    return [elem.strip() for elem in data_str.split(delim)]
+
+def distribute_evenly(number, parts = 4):
+    if not number:
+        return [0] * parts
+
+    result = [number // parts] * parts
+    for i in range(number % parts):
+        result[i] += 1
+    return result
+
+def get_obligatory_shifts(df_input, week):
+    obl_shifts = split_data(df_input)
+    res = set()
+
+    for data in obl_shifts:
+
+        if not data:
+            continue
+        
+        data = data.split('|')
+        if data[2][0] == str(week):
+            res.add((data[0], data[1], tuple(map(int, data[2][2:].split('.')))))
+
+    return res
+
+def reverse_schedule_dict(schedule):
+    reversed_schedule = {}
+    for loc, cab_data in schedule.items():
+        for cab, shifts in cab_data.items():
+            for shift, doctor in shifts.items():
+                if doctor is not None:
+                    if doctor not in reversed_schedule:
+                        reversed_schedule[doctor] = set()
+                    reversed_schedule[doctor].add((loc, cab, shift))
+    return reversed_schedule
+
+
+def calculate_necessary_allocations(df, all_shift_ids, loc_cabs_dict, week, doctor_penalty):
+
+    G = nx.DiGraph()
+    source, sink = 'S', 'T'
+    G.add_node(source, type='source')
+    G.add_node(sink, type='sink')
+    costs = {}
+    cabinet_penalty = {}
+    necessary_shifts = {}
+    schedule = {}
+
+    expected_flow = 0
+
+    for loc in loc_cabs_dict:
+        for spec in loc_cabs_dict[loc]:
+            for cab in loc_cabs_dict[loc][spec]:
+                cabinet_penalty[(loc, cab)] = 0
+                for shift in all_shift_ids:
+                    schedule.setdefault(loc, {}).setdefault(cab, {})
+                    schedule[loc][cab][shift] = None
+                    G.add_node((loc, cab, shift), type='loc_cab_shift')
+                    G.add_edge((loc, cab, shift), sink, capacity=1)
+
+    for _, row in df.iterrows():
+
+        doctor = row['Doctor']
+        costs[doctor] = {}
+
+        specs = split_data(row['Specialization'])
+        locs = split_data(row['Cabinets'])
+
+        min_shifts = distribute_evenly(int(row['MinShifts']) if pd.notna(row['MinShifts']) else 0)[week - 1]
+        expected_flow += min_shifts
+
+        forbidden = set(split_data(row['ForbiddenShifts'])) if pd.notna(row['ForbiddenShifts']) else set()
+        forbidden = set(tuple(map(int, data.split('.')[1:])) for data in forbidden if data[0] == str(week))
+
+        necessary_shifts[doctor] = get_obligatory_shifts(row['RequiredShifts'] if pd.notna(row['RequiredShifts']) else '', week)
+
+        G.add_node(doctor, type='doctor')
+        G.add_edge(source, doctor, capacity=min_shifts)
+
+        for shift in all_shift_ids:
+            if shift in forbidden:
+                continue
+            G.add_node((doctor, shift), type='doctor_shift')
+            G.add_edge(doctor, (doctor, shift), capacity=1)
+
+        for i, loc in enumerate(locs):
+
+            if loc not in costs:
+                costs[loc] = {}
+
+            costs[doctor][loc] = costs[loc][doctor] = 5*i + 1
+
+            for spec in specs:
+                if loc in loc_cabs_dict and spec in loc_cabs_dict[loc]:
+                    for cab in loc_cabs_dict[loc][spec]:
+                        for shift in all_shift_ids:
+                            if shift in forbidden:
+                                continue
+                            G.add_edge((doctor, shift), (loc, cab, shift), capacity=1)
+
+    flow, _, schedule = min_cost_max_flow(G, costs, doctor_penalty, cabinet_penalty, necessary_shifts, schedule, source, sink)
+
+    if flow != expected_flow:
+        print(f"Warning: Expected flow {expected_flow}, but got {flow}. Not all doctors may be assigned their minimum shifts.")
+    else:
+        print('Minimum requirements satisfied')
+    
+    return reverse_schedule_dict(schedule)
+
+
 def generate_preference_schedule_from_csv(input_csv_path: str, loc_cabs_path: str, output_path: str, doctor_penalty: dict, week) -> str:
+
     df = pd.read_csv(input_csv_path)
+
     days, shifts = range(1, 8), range(1, 3)
-    all_shift_ids = [f"{d}.{s}" for d, s in itertools.product(days, shifts)]
-    if doctor_penalty is None:
-        doctors = df['Doctor'].unique()
-        doctor_penalty = {doctor: 0 for doctor in doctors}
-
-
-    def split_data(data_str):
-        parts = []
-        for c in str(data_str).split(','):
-            parts.append(c.strip())
-        return parts
+    all_shift_ids = [(d, s) for d, s in itertools.product(days, shifts)]
 
     with open(loc_cabs_path, 'r', encoding='utf-8') as f:
         loc_cabs_data = json.load(f)
@@ -36,78 +140,64 @@ def generate_preference_schedule_from_csv(input_csv_path: str, loc_cabs_path: st
 
             loc_cabs_dict[loc][spec] = split_data(cabinets)
 
+    required = calculate_necessary_allocations(df, all_shift_ids, loc_cabs_dict, week, doctor_penalty)
+
     G = nx.DiGraph()
     source, sink = 'S', 'T'
-    G.add_node(source)
-    G.add_node(sink)
+    G.add_node(source, type='source')
+    G.add_node(sink, type='sink')
     costs = {}
     cabinet_penalty = {}
-    necessary_shifts = {}
+    schedule = {}
+
+    for loc in loc_cabs_dict:
+        for spec in loc_cabs_dict[loc]:
+            for cab in loc_cabs_dict[loc][spec]:
+                cabinet_penalty[(loc, cab)] = 0
+                for shift in all_shift_ids:
+                    schedule.setdefault(loc, {}).setdefault(cab, {})
+                    schedule[loc][cab][shift] = None
+                    G.add_node((loc, cab, shift), type='loc_cab_shift')
+                    G.add_edge((loc, cab, shift), sink, capacity=1)
 
     for _, row in df.iterrows():
+
         doctor = row['Doctor']
         costs[doctor] = {}
+
         specs = split_data(row['Specialization'])
         locs = split_data(row['Cabinets'])
-        doctor_node = f"D|{doctor}"
-        max_shifts = int(row['MaxShifts']) // 4 if pd.notna(row['MaxShifts']) else len(all_shift_ids) // 4
-        min_shifts = int(row['MinShifts']) //4 if pd.notna(row['MinShifts']) else 0
+
+        max_shifts = distribute_evenly(int(row['MaxShifts']) if pd.notna(row['MaxShifts']) else 4 * len(all_shift_ids))[week - 1]
+
         forbidden = set(split_data(row['ForbiddenShifts'])) if pd.notna(row['ForbiddenShifts']) else set()
-        forbidden = set('.'.join(data.split('.')[1:]) for data in forbidden if data[0] == str(week))
-        obligatory = split_data(row['RequiredShifts']) if pd.notna(row['RequiredShifts']) else set()
-        obligatory = [data.split('|') for data in obligatory if data.split('|')[2][0] == str(week)]
-        obligatory = set('|'.join([data[0], data[1], data[2][2:]]) for data in obligatory)
-        necessary_shifts[doctor] = obligatory
+        forbidden = set(tuple(map(int, data.split('.')[1:])) for data in forbidden if data[0] == str(week))
 
-        if min_shifts > 0:
-            pre_node = f"PRE|{doctor}"
-            G.add_edge(source, pre_node, capacity=min_shifts)
-            G.add_edge(pre_node, doctor_node, capacity=min_shifts)
+        G.add_node(doctor, type='doctor')
+        G.add_edge(source, doctor, capacity=max_shifts)
 
-        if max_shifts > min_shifts:
-            G.add_edge(source, doctor_node, capacity=max_shifts - min_shifts)
+        for shift in all_shift_ids:
+            if shift in forbidden:
+                continue
+            G.add_node((doctor, shift), type='doctor_shift')
+            G.add_edge(doctor, (doctor, shift), capacity=1)
 
         for i, loc in enumerate(locs):
 
-            costs[doctor][loc] = 5*i + 1
             if loc not in costs:
                 costs[loc] = {}
-            costs[loc][doctor] = -costs[doctor][loc]
+
+            costs[doctor][loc] = costs[loc][doctor] = 5*i + 1
 
             for spec in specs:
                 if loc in loc_cabs_dict and spec in loc_cabs_dict[loc]:
                     for cab in loc_cabs_dict[loc][spec]:
-                        cabinet_penalty[f'{loc}|{cab}'] = 0
                         for shift in all_shift_ids:
                             if shift in forbidden:
                                 continue
-                            d_shift_node = f"{doctor}|{shift}"
-                            G.add_edge(doctor_node, d_shift_node, capacity=1)
-                            node = f"{loc}|{cab}|{shift}"
-                            G.add_edge(d_shift_node, node, capacity=1)
-                            G.add_edge(node, sink, capacity=1)
+                            G.add_edge((doctor, shift), (loc, cab, shift), capacity=1)
 
-    _, total_cost, flow_dict = min_cost_max_flow(G, costs, doctor_penalty, cabinet_penalty, necessary_shifts, source, sink)
-
-    schedule = {}
-    for doctor in df['Doctor']:
-        doctor_node = f"D|{doctor}"
-        if doctor_node not in flow_dict:
-            continue
-        
-        for target, flow in flow_dict[doctor_node].items():
-            if flow > 0 and '|' in target:
-                d_shift_node = target
-                for loc_cab_node, flow_value in flow_dict.get(d_shift_node, {}).items():
-                    if flow_value > 0 and '|' in loc_cab_node:
-                        loc, cab, shift = loc_cab_node.split('|')
-                        
-                        if loc not in schedule:
-                            schedule[loc] = {}
-                        if cab not in schedule[loc]:
-                            schedule[loc][cab] = {}
-                        
-                        schedule[loc][cab][shift] = doctor
+    _, _, schedule = min_cost_max_flow(G, costs, doctor_penalty, cabinet_penalty, required, schedule, source, sink)
 
     with open(output_path, "w", encoding="utf-8") as f:
         for loc in sorted(schedule.keys()):
@@ -118,29 +208,187 @@ def generate_preference_schedule_from_csv(input_csv_path: str, loc_cabs_path: st
                 f.write(f"Кабінет: {cab}\n")
                 f.write("-"*30 + "\n")
                 
-                for shift in sorted(all_shift_ids, key=lambda x: list(map(int, x.split('.')))):
+                for shift in all_shift_ids:
                     assigned = schedule[loc][cab].get(shift, "Немає лікаря")
                     f.write(f"{shift} - {assigned}\n")
                 
-                f.write("\n")
-            
-            f.write("\n")
-        
-        f.write(f"Загальна вартість розкладу: {total_cost}\n")
+                f.write("-"*30 + "\n")
     
-    return flow_dict
+    return schedule
 
 def generate_monthly_schedule_from_csv(input_csv_path: str, loc_cabs_path: str, output_path: str) -> str:
+
     df = pd.read_csv(input_csv_path)
-    doctors = df['Doctor'].unique()
-    doctor_penalty = {doctor: 0 for doctor in doctors}
+    doctors = {row['Doctor']: int(row['Fine']) for _, row in df.iterrows()}
+    doctor_penalty = {doctor: 4 if not fine else 0 for doctor, fine in doctors.items()}
+
     for week in range(1, 5):
+
         out_res = output_path + f"week_{week}.txt"
-        flow_dict = generate_preference_schedule_from_csv(input_csv_path, loc_cabs_path, out_res, doctor_penalty, week)
-        for doctor in doctors:
-            doctor_node = f"D|{doctor}"
-            assigned_shifts = sum(flow_dict.get(doctor_node, {}).values())
-            doctor_penalty[doctor] = assigned_shifts // 2
+
+        schedule = generate_preference_schedule_from_csv(input_csv_path, loc_cabs_path, out_res, doctor_penalty, week)
+        for loc in schedule:
+            for cab in schedule[loc]:
+                for shift in schedule[loc][cab]:
+                    if schedule[loc][cab][shift] is not None:
+                        doctor_penalty[schedule[loc][cab][shift]] += 0.5
+        
+        for doctor in doctor_penalty:
+            if not doctors[doctor]:
+                doctor_penalty[doctor] *= 1.2
     
     print('Generated monthly schedule')
+
+
+def change_weekly_schedule(input_csv_path: str, loc_cabs_path: str, weekly_schedule_path: str, deleted_shifts: dict) -> str:
+    df = pd.read_csv(input_csv_path)
+    week = int(weekly_schedule_path.split('_')[-1][0])
+    with open(weekly_schedule_path, 'r', encoding='utf-8') as f:
+        weekly_schedule = f.read().splitlines()
+        current_schedule = {}
+        shifts_to_change = set()
+        necessary_set = set()
+        exp_flow = 0
+
+        current_location, current_cab = None, None
+        for line in weekly_schedule:
+            if line.startswith("Локація:"):
+                current_location = line.replace("Локація: ", "").strip()
+            elif line.startswith("Кабінет:"):
+                current_cab = line.replace("Кабінет: ", "").strip()
+            elif line.startswith("-") or line.startswith("="):
+                continue
+            else:
+                shift, doctor = line.split(" - ")
+
+                if doctor == "None" or doctor == "Немає лікаря":
+                    continue
+
+                exp_flow += 1
+                shift = tuple(map(int, shift[1:-1].split(',')))
+
+                if doctor in deleted_shifts and shift in deleted_shifts[doctor]:
+                    shifts_to_change.add((current_location, current_cab, shift))
+                    continue
+
+                if doctor not in current_schedule:
+                    current_schedule[doctor] = set()
+                
+                current_schedule[doctor].add((current_location, current_cab, shift))
+                necessary_set.add((current_location, current_cab, shift))
+    
+    with open(loc_cabs_path, 'r', encoding='utf-8') as f:
+        loc_cabs_data = json.load(f)
+        loc_cabs_dict = {}
+
+        for elem in loc_cabs_data:
+            loc = elem['location']
+            cabinets = elem['room']
+            spec = elem['specialization']
+            if not loc in loc_cabs_dict:
+                loc_cabs_dict[loc] = {}
+
+            loc_cabs_dict[loc][spec] = split_data(cabinets)
+
+    costs = {}
+    days, shifts = range(1, 8), range(1, 3)
+    all_shift_ids = [(d, s) for d, s in itertools.product(days, shifts)]
+    G = nx.DiGraph()
+    source, sink = 'S', 'T'
+    G.add_node(source, type='source')
+    G.add_node(sink, type='sink')
+    cabinet_penalty = {}
+    doctor_penalty = {}
+    schedule = {}
+
+    for loc in loc_cabs_dict:
+        for spec in loc_cabs_dict[loc]:
+            for cab in loc_cabs_dict[loc][spec]:
+                cabinet_penalty[(loc, cab)] = 0
+                for shift in all_shift_ids:
+                    schedule.setdefault(loc, {}).setdefault(cab, {})
+                    schedule[loc][cab][shift] = None
+
+                    if (loc, cab, shift) in shifts_to_change or (loc, cab, shift) in necessary_set:
+                        G.add_node((loc, cab, shift), type='loc_cab_shift')
+                        G.add_edge((loc, cab, shift), sink, capacity=1)
+
+    doctors = {row['Doctor']: int(row['Fine']) for _, row in df.iterrows()}
+    for doc, fine in doctors.items():
+        doctor_penalty[doc] = len(current_schedule.get(doc, set())) / 2
+        doctor_penalty[doc] += 4 if not fine else 0
+
+    for _, row in df.iterrows():
+
+        doctor = row['Doctor']
+        costs[doctor] = {}
+
+        specs = split_data(row['Specialization'])
+        locs = split_data(row['Cabinets'])
+
+        max_shifts = distribute_evenly(int(row['MaxShifts']) if pd.notna(row['MaxShifts']) else 4 * len(all_shift_ids))[week - 1]
+
+        if doctor in deleted_shifts:
+            max_shifts = len(current_schedule.get(doctor, set()))
+
+        forbidden = set(split_data(row['ForbiddenShifts'])) if pd.notna(row['ForbiddenShifts']) else set()
+        forbidden = set(tuple(map(int, data.split('.')[1:])) for data in forbidden if data[0] == str(week))
+
+        G.add_node(doctor, type='doctor')
+        G.add_edge(source, doctor, capacity=max_shifts)
+
+        for shift in all_shift_ids:
+            if shift in forbidden or (doctor in deleted_shifts and shift in deleted_shifts[doctor]):
+                continue
+            G.add_node((doctor, shift), type='doctor_shift')
+            G.add_edge(doctor, (doctor, shift), capacity=1)
+
+        for i, loc in enumerate(locs):
+
+            if loc not in costs:
+                costs[loc] = {}
+
+            costs[doctor][loc] = costs[loc][doctor] = 5*i + 1
+
+            for spec in specs:
+                if loc in loc_cabs_dict and spec in loc_cabs_dict[loc]:
+                    for cab in loc_cabs_dict[loc][spec]:
+                        for shift in all_shift_ids:
+                            if shift in forbidden or (doctor in deleted_shifts and shift in deleted_shifts[doctor]):
+                                continue
+
+                            if (loc, cab, shift) in shifts_to_change:
+                                G.add_edge((doctor, shift), (loc, cab, shift), capacity=1)
+
+                            if doctor in current_schedule and (loc, cab, shift) in current_schedule[doctor]:
+                                G.add_edge((doctor, shift), (loc, cab, shift), capacity=1)
+
+    flow, _, schedule = min_cost_max_flow(G, costs, doctor_penalty, cabinet_penalty, current_schedule, schedule, source, sink)
+
+    if flow < exp_flow:
+        print(f"Warning: Expected flow {exp_flow}, but got {flow}. Not all shifts have a suitable replacement.")
+
+    with open(weekly_schedule_path, "w", encoding="utf-8") as f:
+        for loc in sorted(schedule.keys()):
+            f.write(f"Локація: {loc}\n")
+            f.write("="*40 + "\n")
+            
+            for cab in sorted(schedule[loc].keys()):
+                f.write(f"Кабінет: {cab}\n")
+                f.write("-"*30 + "\n")
+                
+                for shift in all_shift_ids:
+                    assigned = schedule[loc][cab].get(shift, "Немає лікаря")
+                    f.write(f"{shift} - {assigned}\n")
+                
+                f.write("-"*30 + "\n")
+
+
+if __name__ == "__main__":
+    input_csv_path = "./data/new_data/loc_data_simplified.csv"
+    loc_cabs_path = "./data/new_data/rooms_locations_updated.json"
+    output_path = "./result/"
+    week_path = "./result/week_1.txt"
+    generate_monthly_schedule_from_csv(input_csv_path, loc_cabs_path, output_path)
+    # change_weekly_schedule(input_csv_path, loc_cabs_path, week_path, {'Горічко І.В.': {(3, 1)}})
         
--- new_algo/maximum_flow_impl.py
+++ new_algo/maximum_flow_impl.py
@@ -1,32 +1,44 @@
-from collections import deque, defaultdict
+from collections import defaultdict
 import networkx as nx
-import heapq
 import random
 EPSILON = 1e-5
 PENALTY_MULTIPLIER = 1.5
 
-def bellman_ford(graph, costs, doctor_penalty, cabinet_penalty, necessary_shifts, source, sink):
-    dist = {node: float('inf') for node in graph}
+def bellman_ford(G, residual, costs, doctor_penalty, cabinet_penalty, source, sink):
+    dist = {node: float('inf') for node in residual}
     dist[source] = 0
-    parent = {node: None for node in graph}
+    parent = {node: None for node in residual}
 
-    for _ in range(len(graph) - 1):
-        for u in graph:
-            for v in graph[u]:
-                if not graph[u][v]:
-                    continue
-                first = u.split('|')[0]
-                second = v.split('|')[0] 
-                cost = costs[first][second] if (not (u in necessary_shifts and v in necessary_shifts[u])) and (first in costs and second in costs[first]) else 0
-                if cost:
-                    cab = second + '|' + v.split('|')[1]
-                    penalize_doctor = doctor_penalty[first] if first in doctor_penalty else doctor_penalty[second]
-                    penalize_cabinet = cabinet_penalty[cab] if cab in cabinet_penalty else cabinet_penalty[first + '|' + u.split('|')[1]]
-                    cost += (penalize_doctor + penalize_cabinet) * PENALTY_MULTIPLIER
+    for _ in range(len(residual) - 1):
+        for u in residual:
+            for v in residual[u]:
+                if not residual[u][v]:
+                    continue 
+
+                doctor, loc, cab, cost = None, None, None, 0
+
+                if G.nodes[u].get('type') == 'doctor_shift' and G.nodes[v].get('type') == 'loc_cab_shift':
+                    doctor = u[0]
+                    loc = v[0]
+                    cab = v[1]
+
+                    cost = costs[doctor][loc]
+                    costs[loc][doctor] = cost
+                    cost += (doctor_penalty[doctor] + cabinet_penalty.get((loc, cab), 0)) * PENALTY_MULTIPLIER
+
+                elif G.nodes[u].get('type') == 'loc_cab_shift' and G.nodes[v].get('type') == 'doctor_shift':
+                    doctor = v[0]
+                    loc = u[0]
+                    
+                    cost = -costs[loc][doctor]
+
+                
                 cost += random.uniform(0, EPSILON)
+
                 if dist[u] + cost < dist[v]:
                     dist[v] = dist[u] + cost
                     parent[v] = u
+
     if dist[sink] == float('inf'):
         return None, None
 
@@ -40,7 +52,7 @@ def bellman_ford(graph, costs, doctor_penalty, cabinet_penalty, necessary_shifts
     return dist[sink], path
 
 
-def min_cost_max_flow(G: nx.DiGraph, costs, doctor_penalty, cabinet_penalty, necessary_shifts, source: str, sink: str):
+def min_cost_max_flow(G: nx.DiGraph, costs, doctor_penalty, cabinet_penalty, necessary_shifts, schedule, source: str, sink: str):
     residual = defaultdict(lambda: defaultdict(int))
 
     for u, v, data in G.edges(data=True):
@@ -49,10 +61,23 @@ def min_cost_max_flow(G: nx.DiGraph, costs, doctor_penalty, cabinet_penalty, nec
 
     max_flow = 0
     min_cost = 0
-    flow_dict = defaultdict(dict)
+
+    for doctor in necessary_shifts:
+        for location, cab, shift in necessary_shifts[doctor]:
+            residual[source][doctor] -= 1
+            residual[doctor][(doctor, shift)] -= 1
+            residual[(doctor, shift)][(location, cab, shift)] -= 1
+            residual[(location, cab, shift)][sink] -= 1
+
+            cabinet_penalty[(location, cab)] += 1
+
+            schedule[location][cab][shift] = doctor
+
+        doctor_penalty[doctor] += len(necessary_shifts[doctor])
+        max_flow += len(necessary_shifts[doctor])
 
     while True:
-        _, path = bellman_ford(residual, costs, doctor_penalty, cabinet_penalty, necessary_shifts, source, sink)
+        _, path = bellman_ford(G, residual, costs, doctor_penalty, cabinet_penalty, source, sink)
 
         if path is None:
             break
@@ -61,42 +86,51 @@ def min_cost_max_flow(G: nx.DiGraph, costs, doctor_penalty, cabinet_penalty, nec
         for i in range(len(path) - 1):
             u, v = path[i], path[i + 1]
             path_flow = min(path_flow, residual[u][v])
+    
 
         for i in range(len(path) - 1):
             u, v = path[i], path[i + 1]
             residual[u][v] -= path_flow
             residual[v][u] += path_flow
 
-            first = u.split('|')[0]
-            second = v.split('|')[0]
+            doctor, loc, cab, shift, cost = None, None, None, None, 0
+            reversed = False
+    
+            if G.nodes[u].get('type') == 'doctor_shift' and G.nodes[v].get('type') == 'loc_cab_shift':
+                doctor = u[0]
+                loc, cab, shift = v
 
-            if second[0] != 'D' and first in doctor_penalty:
-                doctor_penalty[first] += 1
-                cab = second + '|' + v.split('|')[1]
-                try:
-                    cabinet_penalty[cab] += 1
-                except KeyError:
-                    raise KeyError(f"Key {cab} not found in cabinet_penalty, {first}, {second}")
+            elif G.nodes[u].get('type') == 'loc_cab_shift' and G.nodes[v].get('type') == 'doctor_shift':
+                doctor = v[0]
+                loc, cab, shift = u
+                reversed = True
 
+            if doctor is not None and loc is not None:
+                # print(f"Processing doctor: {doctor}, loc: {loc}, cab: {cab}, shift: {shift}, reversed: {reversed}")
+                # print(costs[loc])
+                # print(costs[doctor][loc], costs[loc][doctor], doctor_penalty[doctor], cabinet_penalty.get((loc, cab), 0))
 
-            cost = costs[first][second] if first in costs and second in costs[first] else 0
+                if not reversed:
+                    cost = costs[doctor][loc]
+                    cost += (doctor_penalty[doctor] + cabinet_penalty.get((loc, cab), 0)) * PENALTY_MULTIPLIER
+
+                    schedule[loc][cab][shift] = doctor
+                
+                else:
+                    cost = -costs[loc][doctor]
+
+                    schedule[loc][cab][shift] = None
+
+                doctor_penalty[doctor] += 1 if not reversed else -1
+                cabinet_penalty[(loc, cab)] += 1 if not reversed else -1
+
 
             min_cost += cost * path_flow
 
         max_flow += path_flow
-        for i in range(len(path) - 1):
-            u, v = path[i], path[i + 1]
-            if v in G[u]:
-                flow_sent = G[u][v].get('capacity', 0) - residual[u][v]
-                if flow_sent > 0:
-                    flow_dict[u][v] = path_flow
-            else:
-                flow_sent = G[v][u].get('capacity', 0) - residual[v][u]
-                if flow_sent > 0:
-                    flow_dict[v][u] = path_flow
 
 
-    return max_flow, min_cost, dict(flow_dict)
+    return max_flow, min_cost, schedule
 
 
     
